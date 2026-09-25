e72a0d7b70470a3294c57fa758f2b3c61149d7aa
HamidTech-Ventures <hamidtechventures@gmail.com>
2026-09-25 17:33:07 +0500

    Code Cleaned

--- test-pkg.js
+++
@@ -1,6 +0,0 @@
-try {
-  const Database = require('better-sqlite3');
-  console.log("Loaded Database", !!Database);
-} catch (e) {
-  console.error(e);
-}
--- test3.js
+++
@@ -1,8 +0,0 @@
-console.log("Starting backend...");
-try {
-  console.log("Loading better-sqlite3...");
-  const db = require('better-sqlite3');
-  console.log("Loaded better-sqlite3 successfully!");
-} catch (e) {
-  console.error("Failed better-sqlite3", e);
-}
